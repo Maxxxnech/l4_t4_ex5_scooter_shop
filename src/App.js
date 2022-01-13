461e0d1c2ac675e2fb83457662c8dfe0f9b468d8
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-13 09:30:36 +0300

    added buttons for all cards

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import scooters from "./data/scooters.json";
 import Scooter from "./components/Scooter";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { scooters: scooters };
     this.toggleMarked = this.toggleMarked.bind(this);
+    this.markAll = this.markAll.bind(this);
   }
 
   toggleMarked(id) {
@@ -18,11 +20,22 @@ class App extends Component {
     }
     this.setState({ scooters: scootersClone });
   }
+  
+  markAll(mark){
+    const scootersAllMarked = this.state.scooters.map(el => ({...el, marked: mark}) )
+    this.setState({scooters: scootersAllMarked});
+    console.log(this.state.scooters)
+  }
 
   render() {
     return (
       <div className="App">
+        <header>
         <h1>l4_t4, задание 5: прокат самокатов</h1>
+        <button onClick={()=>this.markAll(true)}>Выделить все</button>
+        <button onClick={()=>this.markAll(false)}>Убрать выделение</button>
+        
+        </header>
         <main>
           {this.state.scooters.map((s, i) => (
             <Scooter key={i} data={s} handleClick={this.toggleMarked} />
